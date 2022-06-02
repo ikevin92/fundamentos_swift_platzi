47a97f671274082574743820a25ad4aa237d35ea
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-06-02 00:08:01 -0500

    var and let

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let maximunNumberOfLoginAttemps = 3
+
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+// variable sin valor inicial (Type anotations)
+var welcomeMessage : String
+
+welcomeMessage = "Hi, how are you?"
+
+var red, green, blue : Double
+
+// nomenclatura
+let π = 3.141592
+//var 👽 = "Alien"
+
+//print(👽)
+//👽 = "Alien 2"
+
+// imprimir valores con la funcion print
+print("the number of actually login is \(currentLoginAttempt) of a total available of: \(maximunNumberOfLoginAttemps)")
+
+// Esto es un comentario
+
+
+/*
+ Esto es un comentario
+ Ocupa varias lineas
+y puedo escribir muchas cosas
+ */
+
+/* Comentario padre
+/*  comentario dentro de comentario */
+*/
