6124ee3de84a29d3081990375202f936e1741f3c
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-06-02 00:28:01 -0500

    literales numericos

--- 03_numbers.playground/Contents.swift
+++ 03_numbers.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Enteros
+let age : UInt8 = 31
+
+let minValue8 = UInt8.min  //2ˆ0-1
+let maxValue8 = UInt8.max  //2ˆ8-1
+
+// Double(64b) y Float(32b)
+let f1: Float = 3.14159265
+let d1: Double = 3.14159265
+
+let meanigOfLife = 42 //Int
+let pi = 3.14159 //Double
+let anotherPi = 3 + 0.14159 //Double
+
+// literales numericos
+let decimalInteger = 17 // 1*10 + 7
+let binaryInteger = 0b10001 //binario
+let octalInteger = 0o21 //octal
+let hexadecimalInteger = 0x11
+
+let decimalDouble = 12.1875
+let exponentDouble = 1.21875e1
+let hexadecimalDouble = 0xC.3p0
+
+let paddedDouble = 000123.456
+let someNumbre = 00000097.54
+
+let oneMillion = 1_000_000
+let justMoreThanAMillion = 1_000_000.000_000_1
+
+//let cannotBeNegative: UInt8 = -1
+//let tooBig: UInt8 = UInt8.max + 1
+
